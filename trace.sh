486a5df33861b0cb4fb9c75be8400d2f0fd9d443
ngc104 <yves.mettier@orange.com>
2024-01-19 17:14:56 +0100

    Fixed trace.sh

--- trace.sh
+++ trace.sh
@@ -8,7 +8,7 @@ echo "Traces in '${PPROF_TMPDIR}'"
 
 echo "=======================================" >> "${REPORTDIR}/report.txt"
 echo "${DATE}" >> "${REPORTDIR}/report.txt"
-./alertmanager/alertmanager --version >> "${REPORTDIR}/report.txt"
+./alertmanager --version >> "${REPORTDIR}/report.txt"
 go tool pprof -raw http://localhost:9093/debug/pprof/allocs > /dev/null
 mv "${PPROF_TMPDIR}/pprof.alertmanager.alloc_objects.alloc_space.inuse_objects.inuse_space.001.pb.gz" "${FILEPREFIX}.pb.gz"
 go tool pprof -sample_index=alloc_objects -text "${FILEPREFIX}.pb.gz" | grep "^Showing" >> "${REPORTDIR}/report.txt"
